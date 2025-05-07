ba9df179409ff26d12a2a779df71e64d427f5b64
Michael Soerensen <michaelz_8@hotmail.com>
2025-05-07 13:10:16 +0200

    Developed function to extract data with new ID

--- R/functions.R
+++ R/functions.R
@@ -13,3 +13,40 @@ import_dime <- function(file_path) {
     )
   return(data)
 }
+
+# " Import all DIME csv
+# "
+#' @param folder_path Path to file
+#'
+#' @returns Returns a data frame
+#'
+import_csv_files <- function(folder_path) {
+  files <- folder_path |>
+    fs::dir_ls(glob = "*.csv")
+
+  data <- files |>
+    purrr::map(import_dime) |>
+    purrr::list_rbind(names_to = "file_path_id")
+  return(data)
+}
+
+#' Extract participant id from CSV
+#'
+#' @param data The path to the files
+#'
+#' @returns Returns a data fram
+#'
+get_participant_id <- function(data) {
+  data_with_id <- data |>
+    dplyr::mutate(
+      id = stringr::str_extract(
+        file_path_id,
+        "[:digit:]+\\.csv$"
+      ) |>
+        stringr::str_remove("\\.csv$") |>
+        as.integer(),
+      .before = file_path_id
+    ) |>
+    dplyr::select(-file_path_id)
+  return(data_with_id)
+}
